2da18234f9bd20e63a29bccb56044d460aeec8c6
AzamPutra22 <vectraland15@gmail.com>
2020-12-03 14:59:50 +0700

    azam

---
+++ app/src/main/java/id/ac/poltektegal/uasmpdanoop2/CustomerRoomDatabase.kt
@@ -0,0 +1,18 @@
+package id.ac.poltektegal.uasmpdanoop2
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+
+@Database(entities = arrayOf(Customer::class), version = 1)
+abstract class CustomerRoomDatabase : RoomDatabase() {
+    abstract fun customerDao(): CustomerDao
+
+    abstract val applicationContext: Context
+    val db = Room.databaseBuilder(
+        applicationContext,
+        CustomerRoomDatabase::class.java, "DBCS"
+    ).build()
+
+}
